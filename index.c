e70e98ffa82b1c1dad6ab8359c0001f9e6397143
Helen J Li <helenjli@users.noreply.github.com>
2020-05-02 16:51:20 -0400

    added index.c

---
+++ index.c
@@ -0,0 +1,13 @@
+//
+//  index.c
+//  
+//
+//  Created by Helen Li on 2020-05-02.
+//
+
+#include "index.h"
+#include <stdio.h>
+
+int main(void){
+    printf("Hello world");
+}
---
+++ index.h
@@ -0,0 +1,13 @@
+//
+//  index.h
+//  
+//
+//  Created by Helen Li on 2020-05-02.
+//
+
+#ifndef index_h
+#define index_h
+
+#include <stdio.h>
+
+#endif /* index_h */
